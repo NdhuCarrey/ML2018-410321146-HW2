067d3303592e595adbd080e91795088569a46e25
NdhuCarrey <410321146@gms.ndhu.edu.tw>
2018-07-03 21:50:58 +0800

    update code 3

--- Handwritten Character Recognition.py
+++ Handwritten Character Recognition.py
@@ -9,36 +9,29 @@ import numpy as np
 
 
 (in_train, out_train), (in_test, out_test) = mnist.load_data()
-in_train = in_train.reshape(60000, 784)
-in_test = in_test.reshape(10000, 784)
-in_train = in_train.astype('float32')
-in_test = in_test.astype('float32')
+in_train_MLP = in_train.reshape(60000, 784)
+in_test_MLP = in_test.reshape(10000, 784)
+in_train_MLP = in_train.astype('float32')
+in_test_MLP = in_test.astype('float32')
+in_train_MLP_normalize = in_train / 255
+in_test_MLP_normalize = in_test / 255
 
-in_train_normalize = in_train / 255
-in_test_normalize = in_test / 255
 out_train_onehot = np_utils.to_categorical(out_train)
 out_test_onehot = np_utils.to_categorical(out_test)
 
-print('model1')
-model = Sequential()
-model.add(Dense(256, activation = 'relu', input_shape = 784))
-model.add(Dense(128, activation = 'relu'))
-model.add(Dense(64, activation = 'relu'))
-model.add(Dense(32, activation = 'relu'))
-model.add(Dense(10, activation = 'softmax'))
-model.summary()
 
-model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
-model.fit(in_train_normalize, out_train_onehot, epochs = 20, batch_size = 196)
 
-print(model.evaluate(in_test_normalize, out_test_onehot))
 
-out_pred = model.predict_classes(in_test)
-print(pd.crosstab(out_test, out_pred))
+
+
+in_train_CNN = in_train.reshape(in_train.shape[0], 28, 28, 1).astype('float32')  
+in_test_CNN = in_test.reshape(in_test.shape[0], 28, 28, 1).astype('float32')
+in_train_CNN_normalize = in_train / 255
+in_test_CNN_normalize = in_test / 255
 
 print('model2')
 model2 = Sequential()
-model2.add(Conv2D(8, (3, 3), input_shape= 784, padding='same', activation='relu'))
+model2.add(Conv2D(8, (3, 3), input_shape = (28,28,1), padding='same', activation='relu'))
 model2.add(Dropout(0.25))
 model2.add(MaxPooling2D((2, 2)))
 
@@ -72,7 +65,8 @@ model2.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-print(model2.evaluate(in_test_normalize, out_test_onehot))
+print(model2.evaluate(in_train_CNN_normalize, out_test_onehot))
+model2.fit(in_test_CNN_normalize, out_train_onehot, epochs = 15, batch_size = 196)
 
 out_pred = model2.predict_classes(in_test)
 print(pd.crosstab(out_test, out_pred))
